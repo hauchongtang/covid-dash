612da9c34b961fef32e46a496d555bf7a2ca795a
Hau Chong <thchong-code@users.noreply.github.com>
2020-08-22 20:48:46 +0800

    Updates

---
+++ precache-manifest.d77501bb8851f7619f036b8779c90c1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ae19f05064a8ff7161bff50c844b26",
+    "url": "/covid-dash/index.html"
+  },
+  {
+    "revision": "a1e02b4b789fa1d84466",
+    "url": "/covid-dash/static/css/main.223f409b.chunk.css"
+  },
+  {
+    "revision": "72a6b99c68f4e2aacbfa",
+    "url": "/covid-dash/static/js/2.6a3ef911.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-dash/static/js/2.6a3ef911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1e02b4b789fa1d84466",
+    "url": "/covid-dash/static/js/main.2b33cfaa.chunk.js"
+  },
+  {
+    "revision": "5196286a9a6d820f4ddc",
+    "url": "/covid-dash/static/js/runtime-main.2dd3d4f7.js"
+  }
+]);
