8d5e6996ce0a42590275503a5595b38731270688
Hau Chong <thchong-code@users.noreply.github.com>
2020-09-04 08:35:00 +0800

    styling hotfix

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -10,12 +10,8 @@ const useStyles = makeStyles({
   },
   card: {
     margin: '2% !important',
-    backgroundColor: 'rgb(64,224,208, 0.1)',
-    borderRadius: '12px',
-    borderTop: '12px solid rgb(64,224,208)',
-    width: '80%',
-    marginLeft: '10% !important',
-    textAlign: 'justify'
+    borderColor: 'transparent',
+    textAlign: 'justify',
   },
 })
 
@@ -25,32 +21,32 @@ const About = () => {
     <>
       <TopBar />
       <CardContent elevation={1} className={styles.card}>
-        <Typography align='inherit' variant='h5'>Mentions</Typography>
-        <Button href='https://api.covid19api.com' target="_blank">covid19api</Button>
-        <Button href='https://covid19-api.org' target="_blank">covid19-api</Button>
-        <Button href='https://covid19.mathdro.id' target="_blank">covid19.mathdro.id</Button>
-        <Button href='https://api.coronatracker.com' target="_blank">CoronaTrackerAPI</Button>
-        <Button href='https://api.apify.com' target="_blank">Apify API</Button>
-        <Button href='https://disease.sh/' target="_blank">
+        <Typography align='inherit' variant='h5'>We get our data from</Typography>
+        <Button href='https://api.covid19api.com' target="_blank" color='secondary'>covid19api</Button>
+        <Button href='https://covid19-api.org' target="_blank" color='secondary'>covid19-api</Button>
+        <Button href='https://covid19.mathdro.id' target="_blank" color='secondary'>covid19.mathdro.id</Button>
+        <Button href='https://api.coronatracker.com' target="_blank" color='secondary'>CoronaTrackerAPI</Button>
+        <Button href='https://api.apify.com' target="_blank" color='secondary'>Apify API</Button>
+        <Button href='https://disease.sh/' target="_blank" color='secondary'>
           disease.sh
         </Button>
       </CardContent>
       <CardContent elevation={1} className={styles.card}>
         <Typography align='inherit' variant='h5'>Content</Typography>
-        <Typography variant='button'>Made with React, material-ui and chartjs</Typography>
+        <Button>Made with React, material-ui and chartjs</Button>
       </CardContent>
       <CardContent elevation={1} className={styles.card}>
         <Typography align='inherit' variant='h5'>Code and Contributors</Typography>
-        <Button size='large' href=' https://github.com/thchong-code/covid-dash' target="_blank" color='inherit'>
+        <Button size='large' href=' https://github.com/thchong-code/covid-dash' target="_blank" color='secondary'>
           Github
         </Button>
-        <Button size='large' href='https://github.com/imsamuel' target="_blank" color='inherit'>
+        <Button size='large' href='https://github.com/imsamuel' target="_blank" color='secondary'>
           imsamuel
         </Button>
       </CardContent>
       <CardContent elevation={1} className={styles.card}>
-        <Typography align='inherit' variant='h5'>Find me !</Typography>
-        <Button size='large' href=' https://thchong-code.github.io/portfolio-webpage/#/' target="_blank" color='inherit'>
+        <Typography align='inherit' variant='h5'>Find me @</Typography>
+        <Button size='large' href=' https://thchong-code.github.io/portfolio-webpage/#/' target="_blank" color='secondary'>
           HC
         </Button>
       </CardContent>
